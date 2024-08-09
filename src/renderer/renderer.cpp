680710ff655b5fa624ebd86df15621d3150364f3
Borworntat Dendurongkul <mastericez890@gmail.com>
2024-08-09 20:59:13 +0700

    feat: detect escape

--- src/renderer/renderer.cpp
+++ src/renderer/renderer.cpp
@@ -5,6 +5,18 @@
 
 #include "renderer.hpp"
 
+void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
+	if(width < 1 || height < 1) {
+		std::cout << "WIDTH or HEIGHT is invalid" << std::endl;
+		
+		exit(-1);
+	}
+
+	std::cerr << "Window's size has been changed to (" << width << " x " << height << ")" << std::endl;
+
+	glViewport(0, 0, width, height);
+}
+
 renderer_t::renderer_t(size_t width, size_t height, std::string app_name) {
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -24,6 +36,7 @@ renderer_t::renderer_t(size_t width, size_t height, std::string app_name) {
 	}
 
 	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 	
 	if(!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)) {
 		std::cout << "Failed to initialize GLAD" << std::endl;
--- src/renderer/startApp.cpp
+++ src/renderer/startApp.cpp
@@ -3,8 +3,16 @@
 
 #include "renderer.hpp"
 
+void checkKeyPress(GLFWwindow *window) {
+	if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+		glfwSetWindowShouldClose(window, true);
+	}
+}
+
 void renderer_t::start() {
 	while(!glfwWindowShouldClose(window)) {
+		checkKeyPress(window);
+
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
